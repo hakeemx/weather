ec25651572ee3bd729c522d7d75f90b0f76eb33f
hxk33m <hxk33m@gmail.com>
2017-08-07 19:46:01 -0400

    Fix production bugs

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API_KEY = '4d982e3dcec5f00721a937c65b52ca1b';
-const ROOT_URL= `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL= `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
